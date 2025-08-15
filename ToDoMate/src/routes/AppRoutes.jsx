6684f92bf48f0b3ce3ec6664b79daf1f3e02c071
Sohaila zidan <sohailaziedan533@gmail.com>
2025-08-15 17:49:54 +0300

    Milestone 1 completed: CRUD operations

--- ToDoMate/src/routes/AppRoutes.jsx
+++ ToDoMate/src/routes/AppRoutes.jsx
@@ -1,5 +1,4 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Landing from "../pages/Landing";
 import Login from "../pages/Login";
 import Home from "../pages/Home";
 
@@ -8,9 +7,8 @@ const AppRoutes = () => {
     <div>
        <Router>
       <Routes>
-        <Route path="/" element={<Landing />} />
+        <Route path="/" element={<Home />} />
         <Route path="/login" element={<Login />} />
-        <Route path="/home" element={<Home />} />
       </Routes>
     </Router>
     </div>
